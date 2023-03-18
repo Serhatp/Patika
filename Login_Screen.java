f3df9b23c39df69df4edc5cf5d462d43c9853874
Serhat POLAT <127929222+Serhatp@users.noreply.github.com>
2023-03-18 12:12:25 +0300

    Add files via upload

---
+++ Login_Screen.java
@@ -0,0 +1,57 @@
+package giris;
+import java.util.Scanner;
+
+public class Login_Screen {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Please enter your username: ");
+        String username = input.nextLine();
+        System.out.println("Please enter your password: ");
+        String password = input.nextLine();
+
+
+        if (username.equals("Patika") && password.equals("Java101")){
+            System.out.println("Logging into the system... ");
+            System.out.println("Welcome! ");
+        }
+        else{
+            System.out.println("Username or password is incorrect. ");
+            System.out.println("Do you want to reset your password?\n Please type 1 for yes, 2 for no...");
+            String password_reset = input.nextLine();
+            if (password_reset.equals("1")){
+                System.out.println("Please enter new password: ");
+                String new_password = input.nextLine();
+
+                System.out.println("Please enter username: ");
+                String verify_username = input.nextLine();
+
+                System.out.println("Please enter new password: ");
+                String verify_password = input.nextLine();
+
+                if (verify_username.equals("Patika") && new_password.equals(verify_password)){
+                    System.out.println("Logging in...\nWelcome!");
+                }
+                else{
+                    System.out.println("Incorrect password or username!\nLogging procces was failed...");
+                }
+
+            }
+            if (password_reset.equals("2")){
+                System.out.println("The logging process wasn't successful... ");
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+}}
